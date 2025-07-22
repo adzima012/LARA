dfe910514ce879f875e028b21216c1565393d5a6
Zimm <zimm@Zimms-MacBook-Pro.local>
2025-07-22 15:01:25 +0700

    Benerin bahasa

---
+++ lang/id/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Autentikasi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan selama proses autentikasi untuk berbagai
+    | pesan yang perlu kita tampilkan kepada pengguna. Anda bebas untuk
+    | memodifikasi baris bahasa sesuai dengan keperluan aplikasi.
+    |
+    */
+
+    'failed' => 'Email atau kata sandi yang Anda masukkan salah.',
+    'password' => 'Kata sandi yang dimasukkan salah.',
+    'throttle' => 'Terlalu banyak upaya masuk. Silakan coba lagi dalam :seconds detik.',
+];
